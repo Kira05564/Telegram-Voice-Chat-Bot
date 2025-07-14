355a2b2a36fb0189660aed1894ff4ec57c50e33d
𝗞𝗶𝗿𝗮 <lightyagami0555055@gmail.com>
2025-07-14 13:23:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,8 @@ class Config:
     SESSION_STRING = "BQGNrbcAYZ4vCsH4uyi3h5Hi629bzam7aNlSnzBX5VlynA1i_9XnTe6Or0UssRL9Sb1oQxUvqqfqeFViR562cl7opZ-zXtHsDnmrnlUkHrq9Br-cvasGxei9KkWSWeagGxivtGPtkSccMTyg-n8aaJjQMmEEvI_Hdc4V8v7nLNTuE2eWdbnb82KLSv3tHi8vsShius0XuxBPG0FVwB3znlnNa-fax3OQOi2fTzb-6FvdVS_MfRlsyZQ_C_3S35v8nsJmMmoTfhTLmgkYC7R_GdmFFykRD7qH7AfHEKXWFi-KgrKjlhLRNjQQLwuwGlPABa3UzRh4CVJb65HGDz6iZK-XrJ8kjAAAAAHeVQRbAA"
     ASSISTANT_ID = 8025080923
     OWNER_ID = 7841882010
-    
-    # Database
-    MONGO_DB_URI = "mongodb+srv://rj5706603:O95nvJYxapyDHfkw@cluster0.fzmckei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    
-    # Optional
-    LOG_GROUP_ID = -1001234567890  # For logging
-    SUPPORT_CHAT = "https://t.me/+XCh9FJFoUcAyZjg1"  # Support group username
+
+    MONGO_DB_URI = "mongodb+srv://username:password@cluster0.xxxxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+    LOG_GROUP_ID = -1002607951814
+    SUPPORT_CHAT = "https://t.me/+XCh9FJFoUcAyZjg1"
